125326156693fb65c16c601865974ccc4d25a9a5
547343578 <wanglucas5@gmail.com>
2021-04-14 23:36:14 +0800

    ok

---
+++ js/test.js
@@ -0,0 +1,13 @@
+function changeColour(car, newColour){
+    return car.colour = newColour
+}
+
+function cahngeCar(car){
+    car = {brand:'ferrari', colour: 'Red'}
+    console.log(myCar)
+}
+
+let myCar = {brand:'Volvo', colour:'Grey'}
+console.log(changeColour(myCar,"Blue"))
+cahngeCar(myCar)
+console.log(myCar)
